8df41466c4e0d7a213a910ed6ef1fdbc46bc66a4
Selcuk SERT <selcuk.sert@gmail.com>
2020-08-04 13:16:26 +0300

    Documentation update

--- docker/start.ps1
+++ docker/start.ps1
@@ -1,1 +1,1 @@
-docker-compose -f $PSScriptRoot\docker-images\redhat\pam\docker-compose.yml up -d pam openldap kieapp mavenserver
+docker-compose -f $PSScriptRoot\docker-images\redhat\pam\docker-compose.yml up -d pam openldap mavenserver
